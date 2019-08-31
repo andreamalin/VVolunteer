41c658add3ce4a66217c0c3b7c7bddaf0cf1f074
andreamalin <andreaa.malin@gmail.com>
2019-08-31 09:19:40 -0600

    Actualizacion de Medicamento y LeerMedicamento

--- LeerMedicamento.java
+++ LeerMedicamento.java
@@ -1,17 +1,18 @@
-//Codigo adaptado de https://www.quora.com/How-do-I-read-a-file-line-by-line-in-Java
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class LeerMedicamento {
 	private String txt = ""; //Cada linea de texto se metera a esta variable
 	private String[] lista;
+	ArrayList<String> medicamentos = new ArrayList<String>();
 
-	public void meterMedicamento() {
+	public void meterMedicamento() { //Codigo adaptado de https://www.quora.com/How-do-I-read-a-file-line-by-line-in-Java
 		try {
 			Scanner scanner = new Scanner(new File("medicamento.txt")); //Se lee el archivo de texto
 			while (scanner.hasNextLine()) {
-				txt += (scanner.nextLine()); //Mientras hayan lineas por leer se meten a la variable a
+				txt += (scanner.nextLine()); //Mientras hayan lineas por leer se meten a la variable txt
 			}
 			scanner.close();
 		} catch (FileNotFoundException e) { //Se muestra la razon de error por la que no se encuentra el doc
@@ -19,15 +20,16 @@ public class LeerMedicamento {
 		}
 	}
 
-	public void listaInformativa(){
-		lista = txt.split(", ");
+	public String listaInformativa(){
+		meterMedicamento();
+		lista = txt.split(", "); //Se crea una lista con el nombre, informacion y dosis de los medicamentos
 
-		for (String i : lista) {
-			System.out.println(i);	
-		}
-	}
+		return "";
+    }
 
-	public String getTxt(){
-		return txt;
+	public String[] getLista(){ //Se manda la lista de medicamentos a Medicamento.java
+		listaInformativa();
+
+		return lista;
 	}
 }
--- Medicamento.java
+++ Medicamento.java
@@ -1,16 +1,32 @@
-public class Medicamento{
-	private String informacion = new String();
-	private String dosis = new String();
-	private String similares = new String();
 
-	public String getinformacion(){
-		return informacion;
+public class Medicamento{
+	//Propiedades del medicamento a recetar
+	private String nombre;
+	private String sintoma1;
+	private String sintoma2;
+	private String sintoma3;
+	private String dosis;
+
+	LeerMedicamento medicina = new LeerMedicamento();
+	String [] listamedic = medicina.getLista(); //Se manda a llamar la lista de medicamento
+
+	public boolean buscarMedicamento(String nombremedicamento){
+		for (int i=0; nombremedicamento.equalsIgnoreCase(listamedic[i]); i++) { //Si se encuentra el medicamento
+			this.nombre = nombremedicamento; //Se define el nombre, los tres sintomas y la dosis
+			this.sintoma1 = listamedic[i+1];
+			this.sintoma2 = listamedic[i+2];
+			this.sintoma3 = listamedic[i+3];
+			this.dosis = listamedic[i+4];
+			
+			return true; //Si se encuentra
+		}
+
+		return false; //Si no se encuentra
 	}
-	public String getdosis(){
-		return dosis;
+
+	public String buscarSimilares(){
+		return "";
 	}
-	public String getsimilares(){
-		return similares;
-	}
-	
+
+
 }
