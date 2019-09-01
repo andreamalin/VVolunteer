5cb7e5d7177d0b2b9a5a59071eee743246fd4819
andreamalin <andreaa.malin@gmail.com>
2019-08-31 19:51:13 -0600

    Ya sirven las clases de Medicamento y LeerMedicamento

--- Medicamento.java
+++ Medicamento.java
@@ -6,26 +6,74 @@ public class Medicamento{
 	private String sintoma2;
 	private String sintoma3;
 	private String dosis;
+	private String similares;
 
 	LeerMedicamento medicina = new LeerMedicamento();
 	String [] listamedic = medicina.getLista(); //Se manda a llamar la lista de medicamento
+	String [] nombresmedicamentos;
 
 	public boolean buscarMedicamento(String nombremedicamento){
-		for (int i=0; nombremedicamento.equalsIgnoreCase(listamedic[i]); i++) { //Si se encuentra el medicamento
-			this.nombre = nombremedicamento; //Se define el nombre, los tres sintomas y la dosis
-			this.sintoma1 = listamedic[i+1];
-			this.sintoma2 = listamedic[i+2];
-			this.sintoma3 = listamedic[i+3];
-			this.dosis = listamedic[i+4];
-			
-			return true; //Si se encuentra
+		for (int i=0; i<listamedic.length; i++) { //Si se encuentra el medicamento
+			if (nombremedicamento.equalsIgnoreCase(listamedic[i])) {
+				this.nombre = nombremedicamento; //Se define el nombre, los tres sintomas y la dosis
+				this.sintoma1 = listamedic[i+1];
+				this.sintoma2 = listamedic[i+2];
+				this.sintoma3 = listamedic[i+3];
+				this.dosis = listamedic[i+4];
+				
+				return true; //Si se encuentra	
+			}
 		}
-
 		return false; //Si no se encuentra
 	}
 
-	public String buscarSimilares(){
-		return "";
+	public void separarMed(){
+		int longitud = listamedic.length/5;
+		int a = 0;
+		nombresmedicamentos = new String[longitud];
+
+		for (int i=0; i<longitud; i++) {
+			nombresmedicamentos[i]=listamedic[a];
+
+			a = a+5;
+		}
+	}
+
+	public void buscarSimilares(){
+		separarMed();
+		similares = "                  ";
+		int contador=0;
+
+		for (int i=0; i<nombresmedicamentos.length; i++) {
+			if (sintoma1.equalsIgnoreCase(listamedic[contador+1]) || sintoma2.equalsIgnoreCase(listamedic[contador+2]) || sintoma3.equalsIgnoreCase(listamedic[contador+3])){
+				similares += nombresmedicamentos[i];	
+			}
+			contador=contador+5;
+		}
+		similares = similares.replace(nombre,"");
+
+		if (similares.equalsIgnoreCase("                  ")) {
+			similares = "          No hay medicamento similar";
+		}
+	}
+
+	public String mostrarInformacion(){
+		buscarSimilares();
+
+		String a = "Medicamento no encontrado";
+		if (buscarMedicamento(nombre)) {		
+			a = "\n\n+-----------NOMBRE DEL MEDICAMENTO-----------+\n";
+			a += "                  "+nombre;
+			a += "\n\n+------------SINTOMAS QUE ALIVIA------------+\n";
+			a += "     " + sintoma1 + "     " + sintoma2 + "      " + sintoma3;
+			a += "\n\n+-------------DOSIS RECOMENDADA-------------+\n";
+			a += dosis;
+			a += "\n\n+------------MEDICAMENTO SIMILAR------------+\n";
+			a += similares;
+
+			return a;	
+		}
+		return a;
 	}
 
 
