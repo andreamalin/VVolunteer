f82718b1f9df619967a086b0e424b0325f902a03
andreamalin <andreaa.malin@gmail.com>
2019-09-03 15:40:09 -0600

    Subo la clase de ayudar y reportar

--- Reportar.java
+++ Reportar.java
@@ -1,9 +1,51 @@
+import java.util.Random;
 public class Reportar{
 	private String nombre_usuario;
 	private String correo;
-	private int tel;
+	private String telefono;
 	private String direccion;	
 	private String sintomas;
-	
+	private String[] info;
+	private String informacion;
+	private String respuesta = new String();
+	private int r1;
+	private boolean bandera = false;
+// se crearon variables 
+	public void setInfo(String nom, String cor, int tel, String dire, String sint){
+	// se convierte el numero de telefono a texto para agregarlo al array
+	// de tipo String
+		this.nombre_usuario = nom;
+		this.correo = cor;
+		this.telefono = Integer.toString(tel);
+		this.direccion = dire;
+		this.sintomas = sint;
+	// se guardan los datos en un array de una dimension
+		info = new String[5];
+		info[0] = this.nombre_usuario;
+		info[1] = this.correo;
+		info[2] = this.telefono;
+		info[3] = this.direccion;
+		info[4] = this.sintomas;
+	}
+	// se retorna la informacion
+	public String getInfo(){
+		// se concatenan los datos ingresados por el usuario y con sus especificaciones en una variable de tipo texto para mostrarla
+		informacion = "Nombre de usuario: " + nombre_usuario + " Correo: " + correo + " Direccion: "+ direccion + " Sintomas: " + sintomas+ " Telefono: " + telefono;
+		return informacion;
+	}
+	// se crea un numero aleatorio para verificar si se puede ayudar 
+	// a la persona inmediatamente 
+	public boolean enCamino(){
+		Random r = new Random();
+		r1 = r.nextInt(100);
+		if (r1 == 67){
+			bandera = true;
+		}
+		else{
+			bandera = false;
+		}
+	return bandera;
 
+	}
 }
+
