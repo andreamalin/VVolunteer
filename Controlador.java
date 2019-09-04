2e70f3bcd6e95c92d8f2f2849d89903142df6240
Brandon Hernández <brandonhernandez_10@outlook.es>
2019-09-04 06:52:26 -0600

    Arregle cosas

---
+++ Controlador.java
@@ -0,0 +1,14 @@
+public class Controlador{
+	private Integer opcion;
+	CentroSalud centro = new CentroSalud();
+	
+	public void setOpcion(Integer opcion){
+		this.opcion = opcion;
+	}	
+
+	public Integer getOpcion(){
+		return this.opcion;
+	}
+
+
+}
